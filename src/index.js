1e01e9cad21c118de39def850f4a06898fb5c8bb
Kimala Cochran <kimalajoy@gmail.com>
2020-04-16 19:30:48 -0600

    updated login page and did some styling

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/style.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
-import '../src/images/wil-stewart-KjMy5dLL1s0-unsplash.jpg'
+import '../src/images/wil-stewart-KjMy5dLL1s0-unsplash.jpg';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
