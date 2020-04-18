1fb8012a725041c717629eaea7e4739a6c07c3c1
Kimala Cochran <kimalajoy@gmail.com>
2020-04-18 16:24:10 -0600

    fixed css after name change

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import Manager from '../src/manager.js';
+import Customer from '../src/customer.js';
 
 class App {
   constructor(user, bookings) {
@@ -18,16 +20,45 @@ class App {
   }
 
   login() {
+    this.currentUser = null;
     let username = this.usernameInput.val();
     let password = this.passwordInput.val();
     console.log('login!', username, password);
 
-  
+    if (username === 'manager') {
+      this.currentUser = new Manager();
+      this.loadPage();
+    } else if (username.includes('customer')) {
+      this.currentUser = new Customer(username);
+      this.loadPage();
+    }
+  }
 
-    //this.currentUser
+  logout() {
+    this.currentUser = null;
+    this.loadPage();
+  }
+
+// Hide/unhide section of page as necessary
+  loadPage() {
+    // let loginContainer = $('.login-container')
+    if (this.currentUser instanceof Manager) {
+      $('.login-container').hide();
+      $('#manager-container').show();
+    } else if (this.currentUser instanceof Customer) {
+      $('.login-container').hide();
+      $('#customer-container').show();
+    }
+
   }
 }
 
 export default App;
 
 
+// username: manager
+// password: overlook2020
+// If it is a customer logging in, they should log in with the following credentials:
+
+// username: customer50 (where 50 is the ID of the user)
+// password: overlook2020
